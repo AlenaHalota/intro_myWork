6693e2ee674e086796845c97fe5b1073fb68ba46
Alena <50327758+AlenaHalota@users.noreply.github.com>
2019-07-19 15:02:47 +0200

    Add files via upload

---
+++ Main site/script.js
@@ -0,0 +1,6 @@
+var btnProject = document.getElementById('btn-garden');
+//var containerProject = document.getElementsByClass('extra-details');
+
+btnProject.addEventListener('click', function () {
+		document.querySelector(".extra-details").style.display = "block";
+	});	
